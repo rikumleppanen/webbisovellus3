81cb14a2f3f02eafa7895d30fd08c3289a55cd6a
Leppänen Riku <Riku.Leppanen@almamedia.fi>
2018-01-31 11:46:31 +0200

    EnnenHerokua

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(bodyParser.json())
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+
+app.use(express.static('build'))
 
 let persons = [
   { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -70,14 +74,20 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
+app.put("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const body = request.body;
 
+  const updatedPerson = persons.find((person) => person.id === id);
+  updatedPerson.number = body.number;
 
+  persons = persons.filter((person) => person.id !== id);
+  persons = persons.concat(updatedPerson);
 
-const PORT = 3001
+  response.json(updatedPerson);
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
